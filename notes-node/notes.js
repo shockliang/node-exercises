a1aa14ccb084e07d8f6c0b94635ee3a628178931
Shock Liang <shockwater@gmail.com>
2017-05-05 00:15:31 +0800

    Simplified input with Yargs

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,10 +1,26 @@
 console.log('Starting notes.js');
 
-module.exports.age = 25;
+var addNote = (title, body) => {
+  console.log('Adding note', title, body);
+};
 
-module.exports.addNote = () => {
-  console.log('add note');
-  return 'New note';
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+  console.log('Reading note', title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing note', title);
+}
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
 };
 
 module.exports.add = (a, b) => {
