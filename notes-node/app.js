0fd96628b81358748347b56acbdd243a8b66b398
Shock Liang <shockwater@gmail.com>
2017-05-04 23:44:26 +0800

    Getting input from user

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,21 +1,25 @@
 console.log('Starting app.');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
 const _ = require('lodash');
+const notes = require('./notes.js');
 
+var command = process.argv[2];
+console.log('Command:', command);
+console.log(process.argv);
 
-// console.log(_.isString(true));
-// console.log(_.isString('Shock'));
-
-var filteredArray = _.uniq(['Shock Liang', 1, 'Shock Liang', 2, 3, 4]);
-console.log(filteredArray);
-
-// var res = notes.addNote();
-// console.log(res);
-//
-// console.log('Result:', notes.add(9,-2));
-// var user = os.userInfo();
-// console.log(user);
-// fs.appendFile('greetings.txt', `Hello ${user.username} ! You are ${notes.age}`);
+if(command === 'add') {
+  console.log('Adding new note');
+}
+else if (command === 'list') {
+  console.log('Listing all notes');
+}
+else if (command ==='read') {
+  console.log('Reading note');
+}
+else if (command === 'remove') {
+  console.log('Removing note');
+}
+else {
+  console.log('Command not recognized');
+}
