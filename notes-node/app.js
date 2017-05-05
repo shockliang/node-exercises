ae3a6028042003eefe9f2291b608bdfe648241e8
Shock Liang <shockwater@gmail.com>
2017-05-05 20:51:28 +0800

    Adding and saving notes

--- notes-node/app.js
+++ notes-node/app.js
@@ -9,7 +9,7 @@ const argv = yargs.argv;
 //var command = process.argv[2];
 var command = argv._[0];
 console.log('Command:', command);
-console.log('Process:', process.argv);
+//console.log('Process:', process.argv);
 console.log('Yargs:', argv);
 
 if(command === 'add') {
