7f910b92ac2842b04ed4cda71b3bf6e812a85654
Shock Liang <shockwater@gmail.com>
2017-05-17 20:44:32 +0800

    Listing notes

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -23,7 +21,9 @@ if(command === 'add') {
     }
 }
 else if (command === 'list') {
-    notes.getAll();
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s)`);
+    allNotes.forEach((note) => notes.logNote(note));
 }
 else if (command ==='read') {
     var note = notes.getNote(argv.title);
