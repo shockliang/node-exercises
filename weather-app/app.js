eb40182ef6135bdca98ec309ec5377dd4fadb378
Shock Liang <shockwater@gmail.com>
2017-05-18 22:43:22 +0800

    What's Makes up HTTP request

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,8 @@ request({
     url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    // console.log(JSON.stringify(error, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
