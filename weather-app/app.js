e1bf2b47a6e6f678541952ea78abe3c7a22095ec
Shock Liang <shockwater@gmail.com>
2017-05-22 22:12:06 +0800

    Encoding user input

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .option({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
     // console.log(JSON.stringify(error, undefined, 2));
