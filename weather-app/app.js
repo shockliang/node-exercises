d4f30a5ba758f03d0a9781e4517c6d5b495751a7
Shock Liang <shockwater@gmail.com>
2017-05-22 22:59:20 +0800

    Abstracting callbacks

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
-const request = require('request');
+//const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .option({
@@ -14,19 +15,4 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log('Unable to connect to Google servers');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find that address.');
-    } else if (body.status === 'OK') {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-    }
-});
+geocode.geocodeAddress(argv.address);
