bf8b417829e45b4825d863d8596065c750c0e588
Shock Liang <shockwater@gmail.com>
2017-05-23 23:03:03 +0800

    Get weather data from forecastserver using api.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,24 +1,39 @@
-//const request = require('request');
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
+const request = require('request');
+var cfg = require('./.config/config');
+console.log(JSON.stringify(cfg));
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode');
 
-const argv = yargs
-    .option({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+// const argv = yargs
+//     .option({
+//         a: {
+//             demand: true,
+//             alias: 'address',
+//             describe: 'Address to fetch weather for',
+//             string: true
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if(errorMessage) {
-        console.log(errorMessage);
-    } else {
-        console.log(JSON.stringify(results,undefined, 4));
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if(errorMessage) {
+//         console.log(errorMessage);
+//     } else {
+//         console.log(JSON.stringify(results,undefined, 4));
+//     }
+// });
+
+request({
+    url: `https://api.darksky.net/forecast/${cfg.key}/37.8267,-122.4233`,
+    json: true
+}, (error, response, body) => {
+    if (error) {
+        console.log('Unable to connect to Forecast.io server.')
+    } else if (response.statusCode === 400) {
+        console.log('Unable to fetch weather.');
+    } else if (response.statusCode === 200) {
+        console.log(`Current temprature:${body.currently.temperature}`);
     }
 });
