7064288aa1922fe35fc89925af02573b1be1ca1a
Shock Liang <shockwater@gmail.com>
2017-05-25 22:10:47 +0800

    Add yargs command

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,12 +3,34 @@ const _ = require('lodash');
 const yargs = require('yargs');
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
-//var command = process.argv[2];
+const titleOption = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
+const bodyOption = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+};
+//const argv = yargs.argv;
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOption,
+        body : bodyOption
+    })
+    .command('list', 'list notes')
+    .command('read', 'Read a note', {
+        title: titleOption
+    })
+    .command('remove', 'remove a note', {
+        title: titleOption
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
 var command = argv._[0];
-console.log('Command:', command);
-//console.log('Process:', process.argv);
-console.log('Yargs:', argv);
 
 if(command === 'add') {
     var note = notes.addNote(argv.title, argv.body);
