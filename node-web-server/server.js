58ce0ce7c6e30b6becfe3ce3bb184ba323737c3d
Shock Liang <shockwater@gmail.com>
2017-05-27 20:48:26 +0800

    sending the json back

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,7 +3,14 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello Express</h1>');
+    // res.send('<h1>Hello Express</h1>');
+    res.send({
+        name: 'Shock',
+        likes: [
+            'Basketball',
+            'Gundam'
+        ]
+    })
 });
 
 app.listen(3000);
