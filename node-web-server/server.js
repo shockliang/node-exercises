43756312b8433727b333c554714c1830d4aed1f7
Shock Liang <shockwater@gmail.com>
2017-05-28 01:37:54 +0800

    Express middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
@@ -12,6 +13,19 @@ hbs.registerHelper('screamIt', (text) => {
 });
 
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}:${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
