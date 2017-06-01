d783cb01505fe9057bed88be90025a26cdad3225
Shock Liang <shockwater@gmail.com>
2017-06-01 21:27:51 +0800

    Using assertion module - expect

--- node-test/utils/utils.test.js
+++ node-test/utils/utils.test.js
@@ -1,17 +1,26 @@
 const utils = require('./utils');
+const expected = require('expect');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
 
-    if(res !== 44) {
-        throw new Error(`Expected 44, but got ${res}`)
-    }
+    expected(res).toBe(44).toBeA('number');
 });
 
 it('should square number', () => {
     var res = utils.square(3);
 
-    if(res !== 9) {
-        throw new Error(`Expected 9. but get ${res}`);
-    }
+    expected(res).toBe(9).toBeA('number');
+});
+
+// should verify first and last names are set
+// assert it includes firstName and lastName with proper values
+it('should set first name and last name with proper values', () => {
+    var user = {location:'Anywhere', age:35};
+    var actual = utils.setName(user, 'Shock Liang');
+
+    expected(actual).toInclude({
+        firstName: 'Shock',
+        lastName: 'Liang'
+    });
 });
