8902c5299c0b8140b9ea555da8b3ff0240a37dbe
Shock Liang <shockwater@gmail.com>
2017-06-03 22:18:00 +0800

    Using mongoose validators, types, and default

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -5,24 +5,55 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     complete: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completeAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
 var newTodo = new Todo({
-    text: 'Cook dinner',
-    complete: true,
-    completeAt: 201706032152
+    text:'  Edit this script.  '
 });
 
 newTodo.save().then((doc) => {
     console.log('Saved todo', doc);
 }, (e) => {
     console.log('Unable to save todo',e);
+});
+
+// user
+
+var User = mongoose.model('User', {
+    name: {
+        type: String,
+        require: true,
+        minlength: 1,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+});
+
+var newUser = new User({
+    name: 'Shock',
+    email: 'shock@948794crown.com'
+});
+
+newUser.save().then((doc) => {
+    console.log('Saved user. ', doc);
+}, (e) => {
+    console.log('Unable to save user.', e)
 });
