b35c64aa2a85728edb07b5aba4495f64785ef48e
Shock Liang <shockwater@gmail.com>
2017-06-03 23:48:47 +0800

    Test POST /todos route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 // POST /todos
 app.post('/todos',(req, res) => {
-    console.log(req.body);
+    //console.log(req.body);
     var todo = new Todo({
         text:req.body.text
     });
@@ -28,3 +28,5 @@ app.post('/todos',(req, res) => {
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
+
+module.exports = {app};
