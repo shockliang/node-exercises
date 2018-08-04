16b34c28b07beb545aa0d28f92e43708cd4e2cb8
shocklaing <shockwater@gmail.com>
2018-08-04 17:12:01 +0800

    event arguments.

--- node-module-system/event-module/app.js
+++ node-module-system/event-module/app.js
@@ -2,9 +2,9 @@ const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
 // Register a listener
-emitter.on("messageLogged", function() {
-  console.log("Listener called");
+emitter.on("messageLogged", e => {
+  console.log("Listener called", e);
 });
 
 // Raise an event
-emitter.emit("messageLogged");
+emitter.emit("messageLogged", { id: 1, url: "http://" });
