c5dc10750a57e098586b4c29db0300f7f2669299
shocklaing <shockwater@gmail.com>
2018-08-07 23:11:37 +0800

    Handling http post requests.

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const courses = [
   { id: 1, name: "course1" },
   { id: 2, name: "course2" },
@@ -19,6 +21,16 @@ app.get("/api/courses/:id", (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
   if (!course)
     res.status(404).send("The course with the given id was not found");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+
+  courses.push(course);
   res.send(course);
 });
 
