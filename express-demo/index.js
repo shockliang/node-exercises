55352382b60d9599b392aab8e92b3014f359b31c
shocklaing <shockwater@gmail.com>
2018-08-08 22:39:08 +0800

    Handling http delete requests.

--- express-demo/index.js
+++ express-demo/index.js
@@ -21,17 +21,15 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
   if (!course)
-    res.status(404).send("The course with the given id was not found");
+    return res.status(404).send("The course with the given id was not found");
+    
   res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
   const { error } = validateCourse(req.body);
 
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   const course = {
     id: courses.length + 1,
@@ -46,13 +44,10 @@ app.put("/api/courses/:id", (req, res) => {
   // Look up the course
   const course = courses.find(c => c.id === parseInt(req.params.id));
   if (!course)
-    res.status(404).send("The course with the given id was not found");
+    return res.status(404).send("The course with the given id was not found");
 
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   course.name = req.body.name;
   res.send(course);
@@ -60,6 +55,16 @@ app.put("/api/courses/:id", (req, res) => {
 
 app.get("/api/posts/:year/:month", (req, res) => {
   res.send(req.query);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given id was not found");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
 });
 
 function validateCourse(course) {
