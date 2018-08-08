e53561e5c808375a1ec4dd5aa7b66844cde9e187
shocklaing <shockwater@gmail.com>
2018-08-08 22:13:56 +0800

    Handling http put requests.

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,4 +1,4 @@
-const Joi = require('joi');
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 
@@ -26,19 +26,13 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required()
-  }
+  const { error } = validateCourse(req.body);
 
-  const result = Joi.validate(req.body, schema);
-  console.log(result);
-  
-  if (result.error) {
-    res
-      .status(400)
-      .send(result.error.details[0].message);
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -48,9 +42,35 @@ app.post("/api/courses", (req, res) => {
   res.send(course);
 });
 
+app.put("/api/courses/:id", (req, res) => {
+  // Look up the course
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given id was not found");
+
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
 app.get("/api/posts/:year/:month", (req, res) => {
   res.send(req.query);
 });
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+
+  return Joi.validate(course, schema);
+}
 
 // PORT
 const prot = process.env.PORT || 3000;
