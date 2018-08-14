b91c1737cea46c4d803addd6d178a23ef31945e6
shocklaing <shockwater@gmail.com>
2018-08-14 22:33:57 +0800

    Flatten the callback hell using named functions.

--- asynchronous-javascript/index.js
+++ asynchronous-javascript/index.js
@@ -1,25 +1,40 @@
 console.log("Before");
 
-getUser(1, user => {
-  console.log(user);
-  
-  getRepositories(user.githubUsername, repos => {
-    console.log(repos);
-  });
-});
+getUser(1, getRepositoriesFromUser);
 
 console.log("After");
 
+function displayCommits(commits) {
+  console.log(commits);
+}
+
+function getCommitsFromRepo(repos) {
+  console.log(repos);
+  getCommits(repos[0], displayCommits);
+}
+
+function getRepositoriesFromUser(user) {
+  console.log(user);
+  getRepositories(user.githubUsername, getCommitsFromRepo);
+}
+
 function getUser(id, callback) {
   setTimeout(() => {
-    console.log("Reading a user from a database");
+    console.log(`Reading the user id:${id} from a database`);
     callback({ id: id, githubUsername: "shock" });
-  }, 2000);
+  }, 1000);
 }
 
 function getRepositories(username, callback) {
   setTimeout(() => {
     console.log(`Reading the ${username}'s repositories...`);
     callback(["repo1", "repo2", "repo3"]);
-  }, 2000);
+  }, 1000);
+}
+
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    console.log(`get commits from ${repo}`);
+    callback(["commit 1", "commit 2", "commit 3"]);
+  }, 1000);
 }
