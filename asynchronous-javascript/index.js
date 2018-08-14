f30dfea5a3e8bbed84eca635152515517e1f298d
shocklaing <shockwater@gmail.com>
2018-08-14 22:05:26 +0800

    Using callbacks for async code.

--- asynchronous-javascript/index.js
+++ asynchronous-javascript/index.js
@@ -1,5 +1,25 @@
 console.log("Before");
-setTimeout(() => {
-  console.log("Reading a user from a database");
-}, 2000);
+
+getUser(1, user => {
+  console.log(user);
+  
+  getRepositories(user.githubUsername, repos => {
+    console.log(repos);
+  });
+});
+
 console.log("After");
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading a user from a database");
+    callback({ id: id, githubUsername: "shock" });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log(`Reading the ${username}'s repositories...`);
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+}
