0af8224137f7b9245bd70c64035d8f03fe1bb078
shocklaing <shockwater@gmail.com>
2018-08-15 22:26:44 +0800

    Rolling back index.js to callback hell mode.

--- asynchronous-javascript/index.js
+++ asynchronous-javascript/index.js
@@ -1,40 +1,30 @@
-console.log("Before");
-
-getUser(1, getRepositoriesFromUser);
-
-console.log("After");
-
-function displayCommits(commits) {
-  console.log(commits);
-}
-
-function getCommitsFromRepo(repos) {
-  console.log(repos);
-  getCommits(repos[0], displayCommits);
-}
-
-function getRepositoriesFromUser(user) {
-  console.log(user);
-  getRepositories(user.githubUsername, getCommitsFromRepo);
-}
+console.log('Before');
+getUser(1, (user) => {
+  getRepositories(user.gitHubUsername, (repos) => {
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    })
+  })
+});
+console.log('After');
 
 function getUser(id, callback) {
   setTimeout(() => {
-    console.log(`Reading the user id:${id} from a database`);
-    callback({ id: id, githubUsername: "shock" });
-  }, 1000);
+    console.log('Reading a user from a database...');
+    callback({ id: id, gitHubUsername: 'mosh' });
+  }, 2000);
 }
 
 function getRepositories(username, callback) {
   setTimeout(() => {
-    console.log(`Reading the ${username}'s repositories...`);
-    callback(["repo1", "repo2", "repo3"]);
-  }, 1000);
+    console.log('Calling GitHub API...');
+    callback(['repo1', 'repo2', 'repo3']);
+  }, 2000);
 }
 
 function getCommits(repo, callback) {
   setTimeout(() => {
-    console.log(`get commits from ${repo}`);
-    callback(["commit 1", "commit 2", "commit 3"]);
-  }, 1000);
+    console.log('Calling GitHub API...');
+    callback(['commit']);
+  }, 2000);
 }
