dd592e2632a62a84df704f1d6b6cd953888bc7b8
shocklaing <shockwater@gmail.com>
2018-08-16 22:45:55 +0800

    Running parallel promises.

--- asynchronous-javascript/promise-api.js
+++ asynchronous-javascript/promise-api.js
@@ -1,5 +1,19 @@
-const pResolve = Promise.resolve({ id: 1 });
-pResolve.then(result => console.log(result));
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Async operation 1...");
+    resolve(1);
+    // reject(new Error('Something failed.'))
+  }, 2000);
+});
 
-const pReject = Promise.reject(new Error('reason for rejection...'));
-pReject.catch(error => console.log('Error', error));
+const p2 = new Promise(resolve => {
+  setTimeout(() => {
+    console.log("Async operation 2...");
+    resolve(2);
+  }, 2000);
+});
+
+// Promise.all([p1, p2])
+Promise.race([p1, p2])
+  .then(result => console.log(result))
+  .catch(error => console.log('Error', error.message));
