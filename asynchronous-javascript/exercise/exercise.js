d57bbe5a26e7afa2307f2f681f029ea88c9aaccf
shocklaing <shockwater@gmail.com>
2018-08-17 23:32:14 +0800

    Solution for exercise.

--- asynchronous-javascript/exercise/exercise.js
+++ asynchronous-javascript/exercise/exercise.js
@@ -1,35 +1,53 @@
+// getCustomer(1, customer => {
+//   console.log("Customer: ", customer);
+//   if (customer.isGold) {
+//     getTopMovies(movies => {
+//       console.log("Top movies: ", movies);
+//       sendEmail(customer.email, movies, () => {
+//         console.log("Email sent...");
+//       });
+//     });
+//   }
+// });
 
-getCustomer(1, (customer) => {
-  console.log('Customer: ', customer);
-  if (customer.isGold) {
-    getTopMovies((movies) => {
-      console.log('Top movies: ', movies);
-      sendEmail(customer.email, movies, () => {
-        console.log('Email sent...')
-      });
-    });
+notifyTopMovies();
+
+async function notifyTopMovies() {
+  const customer = await getCustomer(1);
+  console.log("Customer: ", customer);
+  const movies = customer.isGold ? await getTopMovies() : null;
+  console.log("Top movies: ", movies);
+  if (movies !== null) {
+    await sendEmail(customer.email, movies);
+    console.log("Email sent...");
   }
-});
-
-function getCustomer(id, callback) {
-  setTimeout(() => {
-    callback({ 
-      id: 1, 
-      name: 'Mosh Hamedani', 
-      isGold: true, 
-      email: 'email' 
-    });
-  }, 4000);  
 }
 
-function getTopMovies(callback) {
-  setTimeout(() => {
-    callback(['movie1', 'movie2']);
-  }, 4000);
+function getCustomer(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        id: id,
+        name: "Mosh Hamedani",
+        isGold: true,
+        email: "email"
+      });
+    }, 2000);
+  });
 }
 
-function sendEmail(email, movies, callback) {
-  setTimeout(() => {
-    callback();
-  }, 4000);
+function getTopMovies() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(["movie1", "movie2"]);
+    }, 2000);
+  });
+}
+
+function sendEmail(email, movies) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
 }
