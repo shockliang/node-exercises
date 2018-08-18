7107cc5fce4966f8ef903198a1ec741052a60b22
shocklaing <shockwater@gmail.com>
2018-08-18 14:53:04 +0800

    Saving a document.

--- crud-using-mongodb/index.js
+++ crud-using-mongodb/index.js
@@ -14,9 +14,17 @@ const courseSchema = new mongoose.Schema({
 });
 
 const Course = mongoose.model("Course", courseSchema);
-const course = new Course({
-  name: "Node.js course",
-  author: "Mosh",
-  tags: ["node", "backend"],
-  isPublished: true
-});
+
+async function createCourse() {
+  const course = new Course({
+    name: "Angular course",
+    author: "Mosh",
+    tags: ["angular", "frontend"],
+    isPublished: true
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
