fa92d434ee89302b4d6a49dd42a145c1bead71a1
shocklaing <shockwater@gmail.com>
2018-08-18 14:32:54 +0800

    Creating course schema.

--- crud-using-mongodb/index.js
+++ crud-using-mongodb/index.js
@@ -4,3 +4,11 @@ mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("Connected to MongoDb..."))
   .catch(error => console.log("Could not connect to MongoDb", error));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
--- crud-using-mongodb/readme.md
+++ crud-using-mongodb/readme.md
@@ -6,3 +6,15 @@
     * `sudo chown -R `id -un` /data/db`
 * Run the mongodb daemon
     * `mongod`
+
+* Schema types
+    * String
+    * Number
+    * Date
+    * Buffer
+        * Stored binary data
+    * Boolean
+    * ObjectID
+        * For assign unique identity.
+    * Array
+
