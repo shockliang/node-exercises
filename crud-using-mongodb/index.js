40e6c381016c1a9a3e2f0ed33ea5896e0c656d10
shocklaing <shockwater@gmail.com>
2018-08-18 15:17:46 +0800

    Querying with regular expressions.

--- crud-using-mongodb/index.js
+++ crud-using-mongodb/index.js
@@ -30,11 +30,9 @@ async function createCourse() {
 async function getCourses() {
   const courses = await Course
     // .find({ author: "Mosh", isPublished: true })
-    // .find({ price: { $gte: 10, $lte: 20 } }) // the price should be 10 to 20 dollars.
-    // .find({ price: { $in: [10, 15, 20] } }) // the price should be 10 or 15 or 20 dollars.
-    .find()
-    // .or([{ author: "Mosh" }, { isPublished: true }])
-    .and([{ author: "Mosh" }, { isPublished: true }])
+    .find({ author: /^Mosh/ }) // Starts with Mosh
+    .find({ author: /Hamedani$/i }) // End with Hamedani
+    .find({ author: /.*Mosh.*/i }) // Contains Mosh
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
