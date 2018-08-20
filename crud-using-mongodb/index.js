92dcc923ab8ee14c681b469f392c3aea3974f046
shocklaing <shockwater@gmail.com>
2018-08-20 22:24:04 +0800

    Custom validators.

--- crud-using-mongodb/index.js
+++ crud-using-mongodb/index.js
@@ -18,7 +18,15 @@ const courseSchema = new mongoose.Schema({
     enum: ["web", "mobile", "network"]
   },
   author: String,
-  tags: [String],
+  tags: {
+    type: Array,
+    validate: {
+      validator: function(v) {
+        return v && v.length > 0;
+      },
+      message: "A course should have at least one tag."
+    }
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
   price: {
@@ -36,9 +44,10 @@ const Course = mongoose.model("Course", courseSchema);
 async function createCourse() {
   const course = new Course({
     name: "Angular course",
-    category: "-",
+    category: "web",
     author: "Mosh",
-    tags: ["angular", "frontend"],
+    // tags: ["angular", "frontend"],
+    tags: [],
     isPublished: true,
     price: 15
   });
