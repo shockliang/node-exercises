bb0e5df0d626245fb51e40ad87110bb1c5ad5549
shocklaing <shockwater@gmail.com>
2018-08-24 17:52:16 +0800

    Restructuring genre api.

---
+++ vidly/models/genre.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const Genre = mongoose.model(
+  "Genre",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50
+    }
+  })
+);
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string()
+      .min(5)
+      .required()
+  };
+
+  return Joi.validate(genre, schema);
+}
+
+module.exports.Genre = Genre;
+module.exports.validate = validateGenre;
