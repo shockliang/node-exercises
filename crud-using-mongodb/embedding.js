80fa5573a9b7e4b38bbf5316cd51c0942092d3b2
shocklaing <shockwater@gmail.com>
2018-08-26 19:10:51 +0800

    Using an array of sub-document.

--- crud-using-mongodb/embedding.js
+++ crud-using-mongodb/embedding.js
@@ -17,17 +17,14 @@ const Course = mongoose.model(
   "Course",
   new mongoose.Schema({
     name: String,
-    author: {
-      type: authorSchema,
-      required: true
-    }
+    authors: [authorSchema]
   })
 );
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name,
-    author
+    authors
   });
 
   const result = await course.save();
@@ -56,6 +53,25 @@ async function updateAuthor(courseId) {
   // course.save();
 }
 
-// createCourse('Node Course', new Author({ name: 'Mosh' }));
+async function addAduthor(courseId, author) {
+  const course = await Course.findById(courseId);
+  course.authors.push(author);
+  course.save();
+}
 
-updateAuthor("5b80fea6961cab438aa883b0");
+async function removeAuthor(courseId, authorId) {
+  const course = await Course.findById(courseId);
+  const author = course.authors.id(authorId);
+  author.remove();
+  course.save();
+}
+
+// createCourse("Node Course", [
+//   new Author({ name: "Mosh" }),
+//   new Author({ name: "Shock" })
+// ]);
+
+// addAduthor("5b8288f756a63052cb9a9abd", new Author({ name: "John" }));
+removeAuthor("5b8288f756a63052cb9a9abd", "5b8289ab7e787353b2bdd01e");
+
+// updateAuthor("5b80fea6961cab438aa883b0");
