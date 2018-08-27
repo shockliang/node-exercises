7b7c1cf5b93f671a83ef8d98cad928e48c967db3
shocklaing <shockwater@gmail.com>
2018-08-27 22:48:33 +0800

    Implementing rentals api.

---
+++ vidly/models/rental.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const Rental = mongoose.model(
+  "Rental",
+  new mongoose.Schema({
+    customer: {
+      type: new mongoose.Schema({
+        name: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 50
+        },
+        isGold: {
+          type: Boolean,
+          default: false
+        },
+        phone: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 50
+        }
+      }),
+      required: true
+    },
+    movie: {
+      type: new mongoose.Schema({
+        title: {
+          type: String,
+          required: true,
+          trim: true,
+          minlength: 5,
+          maxlength: 50
+        },
+        dailyRentalRate: {
+          type: Number,
+          required: true,
+          min: 0,
+          max: 255
+        }
+      }),
+      required: true
+    },
+    dateOut: {
+      type: Date,
+      require: true,
+      default: Date.now
+    },
+    dateReturned: {
+      type: Date
+    },
+    rentalFee: {
+      type: Number,
+      min: 0
+    }
+  })
+);
+
+function validateRental(rental) {
+  const schema = {
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required()
+  };
+  return Joi.validate(rental, schema);
+}
+
+module.exports.Rental = Rental;
+module.exports.validate = validateRental;
