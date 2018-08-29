e314373471a17d70bbc2c11954a176a2503104e4
shocklaing <shockwater@gmail.com>
2018-08-29 22:46:04 +0800

    Using lodash for pick fields

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -1,5 +1,6 @@
 const { User, validate } = require("../models/user");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
 
@@ -10,14 +11,11 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered.");
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
 
   await user.save();
-  res.send(user);
+
+  res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
