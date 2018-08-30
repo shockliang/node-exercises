5794c0a7d0b1b5fb9d57bb0ed01ff4867dde21bc
shocklaing <shockwater@gmail.com>
2018-08-30 15:42:22 +0800

    Hashing passwords.

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -1,6 +1,7 @@
+const _ = require("lodash");
 const { User, validate } = require("../models/user");
 const mongoose = require("mongoose");
-const _ = require("lodash");
+const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
 
@@ -12,6 +13,8 @@ router.post("/", async (req, res) => {
   if (user) return res.status(400).send("User already registered.");
 
   user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
 
