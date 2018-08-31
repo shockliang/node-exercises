78591ad9cd67337ebe711cbe95816ae86a3ce796
shocklaing <shockwater@gmail.com>
2018-08-31 21:02:07 +0800

    Storing secrets in environment variables.

--- vidly/routes/auth.js
+++ vidly/routes/auth.js
@@ -1,6 +1,7 @@
 const _ = require("lodash");
 const Joi = require("joi");
 const jwt = require("jsonwebtoken");
+const config = require("config");
 const { User } = require("../models/user");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
@@ -17,7 +18,7 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password");
 
-  const token = jwt.sign({ _id: user._id }, 'jwtImportKey');
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
   res.send(token);
 });
 
