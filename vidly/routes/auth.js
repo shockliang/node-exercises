48936456cdf728f981c8e1296d9c87d28d8b9f0b
shocklaing <shockwater@gmail.com>
2018-08-31 21:23:47 +0800

    Encapsulating logic in mongoose models.

--- vidly/routes/auth.js
+++ vidly/routes/auth.js
@@ -1,7 +1,5 @@
 const _ = require("lodash");
 const Joi = require("joi");
-const jwt = require("jsonwebtoken");
-const config = require("config");
 const { User } = require("../models/user");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
@@ -18,7 +16,7 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password");
 
-  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+  const token = user.generateAuthToken();
   res.send(token);
 });
 
