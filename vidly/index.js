cbf45ef29a0fbca99b1a01a169e75c2bd4ecff5c
shocklaing <shockwater@gmail.com>
2018-09-01 18:00:40 +0800

    Express error middleware.

--- vidly/index.js
+++ vidly/index.js
@@ -2,6 +2,7 @@ const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const config = require("config");
 const mongoose = require("mongoose");
+const error = require('./middlewares/error');
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
@@ -28,6 +29,7 @@ app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -4,9 +4,13 @@ const { Genre, validate } = require("../models/genre");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const genres = await Genre.find().sort("name");
-  res.send(genres);
+router.get("/", async (req, res, next) => {
+  try {
+    const genres = await Genre.find().sort("name");
+    res.send(genres);
+  } catch (ex) {
+    next(ex);
+  }
 });
 
 router.post("/", auth, async (req, res) => {
