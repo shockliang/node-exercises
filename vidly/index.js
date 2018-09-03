9faf957fe50f2c6bcf0a6b9e22d512d088e7cd58
shocklaing <shockwater@gmail.com>
2018-09-03 20:19:44 +0800

    Refactoring index.js extracting routes.

--- vidly/index.js
+++ vidly/index.js
@@ -5,16 +5,11 @@ const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const config = require("config");
 const mongoose = require("mongoose");
-const error = require("./middlewares/error");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
+
 const express = require("express");
 require("express-async-errors");
 const app = express();
+require("./startup/routes")(app);
 
 process.on("unhandledRejection", ex => {
   throw ex;
@@ -55,15 +50,6 @@ mongoose
   .connect("mongodb://localhost/vidly")
   .then(() => console.log("Connected to MongoDb..."))
   .catch(error => console.log("Could not connect to MongoDb", error));
-
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
