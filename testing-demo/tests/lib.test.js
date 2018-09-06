98d2c949138b632c5372d59dd97e4209b6e1d8b3
shocklaing <shockwater@gmail.com>
2018-09-06 21:54:32 +0800

    Testing string.

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,4 +1,5 @@
 const lib = require("../lib");
+
 describe("absolute", () => {
   it("should return a positive number if input is positive", () => {
     const result = lib.absolute(1);
@@ -13,5 +14,13 @@ describe("absolute", () => {
   it("should return 0 if input is 0", () => {
     const result = lib.absolute(-1);
     expect(result).toBe(1);
+  });
+});
+
+describe("greet", () => {
+  it("should return the greeting message", () => {
+    const result = lib.greet("shock");
+    // expect(result).toMatch(/shock/); regex match
+    expect(result).toContain("shock");
   });
 });
