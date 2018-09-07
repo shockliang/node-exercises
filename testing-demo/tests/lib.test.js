9aa899c21702546e02f2413188f1c793512e73e9
shocklaing <shockwater@gmail.com>
2018-09-07 20:11:54 +0800

    Testing array.

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -24,3 +24,11 @@ describe("greet", () => {
     expect(result).toContain("shock");
   });
 });
+
+describe("getCurrencies", () => {
+  it("should return supported currencies", () => {
+    const result = lib.getCurrencies();
+
+    expect(result).toEqual(expect.arrayContaining(["EUR", "USD", "AUD"]));
+  });
+});
