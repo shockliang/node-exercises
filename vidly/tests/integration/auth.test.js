32957bbf7b1434348fd00101a146e98e3e7151df
shocklaing <shockwater@gmail.com>
2018-09-10 21:14:10 +0800

    Testing the auth middleware.

---
+++ vidly/tests/integration/auth.test.js
@@ -0,0 +1,43 @@
+const request = require("supertest");
+const { User } = require("../../models/user");
+const { Genre } = require("../../models/genre");
+
+describe("auth middleware", () => {
+  let token;
+
+  beforeEach(() => {
+    server = require("../../index");
+    token = new User().generateAuthToken();
+  });
+
+  afterEach(async () => {
+    await Genre.remove({});
+    server.close();
+  });
+
+  const exec = () => {
+    return request(server)
+      .post("/api/genres")
+      .set("x-auth-token", token)
+      .send({ name: "genre1" });
+  };
+
+  it("should return 401 if no token is provided", async () => {
+    token = '';
+    const res = await exec();
+    expect(res.status).toBe(401);
+  });
+
+  it("should return 400 if no token is invalid", async () => {
+    token = 'a';
+    const res = await exec();
+    expect(res.status).toBe(400);
+  });
+
+  it("should return 200 if no token is valid", async () => {
+    const res = await exec();
+    expect(res.status).toBe(200);
+  });
+
+  
+});
