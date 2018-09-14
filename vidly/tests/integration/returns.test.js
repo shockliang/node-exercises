aa9357ed7d06ca2440562feb37aaf6050eba2ae4
shocklaing <shockwater@gmail.com>
2018-09-14 21:26:32 +0800

    Initial returns api test for TDD.

---
+++ vidly/tests/integration/returns.test.js
@@ -0,0 +1,41 @@
+const { Rental } = require("../../models/rental");
+const mongoose = require("mongoose");
+
+describe("/api/returns", () => {
+  let server;
+  let customerId;
+  let movieId;
+  let rental;
+
+  beforeEach(async () => {
+    server = require("../../index");
+
+    customerId = mongoose.Types.ObjectId();
+    movieId = mongoose.Types.ObjectId();
+
+    rental = new Rental({
+      customer: {
+        _id: customerId,
+        name: "12345",
+        phone: "12345"
+      },
+      movie: {
+        _id: movieId,
+        title: "12345",
+        dailyRentalRate: 2
+      }
+    });
+
+    await rental.save();
+  });
+
+  afterEach(async () => {
+    await Rental.remove({});
+    server.close();
+  });
+
+  it("should work", async () => {
+    const result = await Rental.findById(rental._id);
+    expect(result).not.toBeNull();
+  });
+});
