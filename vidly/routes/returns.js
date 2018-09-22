d59f0eca80273c7b3d28fb15ac6b4d2d83aa3ef5
shocklaing <shockwater@gmail.com>
2018-09-22 12:22:49 +0800

    Mongoose static methods.

--- vidly/routes/returns.js
+++ vidly/routes/returns.js
@@ -9,10 +9,7 @@ const router = express.Router();
 const winston = require("winston");
 
 router.post("/", [auth, validate(validateReturn)], async (req, res) => {
-  const rental = await Rental.findOne({
-    "customer._id": req.body.customerId,
-    "movie._id": req.body.movieId
-  });
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
   if (!rental) return res.status(404).send("Rental not found.");
 
