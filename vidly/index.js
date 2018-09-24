03c254e9d4a3a9e078d5a5f805ddc7880bb3919e
shocklaing <shockwater@gmail.com>
2018-09-24 15:01:36 +0800

    Preparing the app for production setting.

--- vidly/index.js
+++ vidly/index.js
@@ -7,7 +7,10 @@ require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/validation")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
 module.exports = server;
